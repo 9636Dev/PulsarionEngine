351d07335dcd0b6852279667c1d3f7f87c7ac36e
9636Dev <9636dev@gmail.com>
2024-02-20 21:40:57 +0100

    [Submodule Update] Pulsarion Windowing

--- PulsarionExamples/src/Application.cpp
+++ PulsarionExamples/src/Application.cpp
@@ -4,12 +4,33 @@
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
     using namespace Pulsarion::Windowing;
-    WindowCreationData creationData("Pulsarion Window", 800, 600);
+    WindowCreationData creationData("Pulsarion Window", 800, 600, 0, 0, WindowFlags::DefaultNoVisible | WindowFlags::Resizable);
     auto window = CreateUniqueWindow(creationData);
+    window->LimitEvents(argc == 2);
+
+    window->SetOnClose([]() {
+        PULSARION_LOG_INFO("Window close event received");
+        return true;
+    });
+
+    // When the SetVisible method is called, the window will be shown and the OnWindowVisibility event will be triggered.
+    window->SetOnWindowVisibility([](bool visible) {
+        PULSARION_LOG_INFO("Window visibility event received: {}", visible ? "visible" : "hidden");
+    });
+
+    window->SetOnFocus([](bool focused) {
+        PULSARION_LOG_INFO("Window focus event received: {}", focused ? "focused" : "unfocused");
+    });
+
+    window->SetOnResize([](int width, int height) {
+        PULSARION_LOG_INFO("Window resize event received: {}x{}", width, height);
+    });
+
     window->SetVisible(true);
 
     while (!window->ShouldClose())
     {
+        PULSARION_LOG_INFO("New frame");
         window->PollEvents();
     }
 
