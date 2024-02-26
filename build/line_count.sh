3e7d2cab1c980a6c9e6a874e8c657fed7ec9a29d
9636Dev <9636dev@gmail.com>
2024-02-26 08:16:35 +0100

    Modified line_count.sh to properly exclude files

--- build/line_count.sh
+++ build/line_count.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 cd ../
 
 # Regular expression to exclude specific directories
-EXCLUDE_DIRS='build|PulsarionWindowing/vendor|PulsarionMedia/vendor|PulsarionCore/vendor|PulsarionMath/vendor'
+EXCLUDE_DIRS='build|PulsarionBenchmarks/vendor|PulsarionMedia/vendor|PulsarionCore/vendor|PulsarionMath/tests/vendor|PulsarionShaderLanguage/tests/vendor'
 
 # Run CLOC with regular expression exclusion
 cloc --fullpath --not-match-d="$EXCLUDE_DIRS" --by-file --csv --quiet --report-file=build/line_count.csv .
