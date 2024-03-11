e254903b04f1ad13f011a6382b91f0c134737f6f
9636Dev <9636dev@gmail.com>
2024-03-11 16:14:36 +0100

    Updated shader tests

--- PulsarionExamples/src/Application.cpp
+++ PulsarionExamples/src/Application.cpp
@@ -8,12 +8,13 @@
 
 #include <chrono>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
 {
     using namespace Pulsarion::Windowing;
-    constexpr DebugOptions options  = { false, true, true, true, true };
+    expr DebugOptions options = {false, true, true, true, true};
     WindowBounds bounds(0, 0, 1280, 720);
-    WindowStyles styles = WindowStyles::NSTitled | WindowStyles::NSClosable | WindowStyles::NSMiniaturizable | WindowStyles::NSResizable;
+    WindowStyles styles = WindowStyles::NSTitled | WindowStyles::NSClosable | WindowStyles::NSMiniaturizable |
+                          WindowStyles::NSResizable;
     WindowConfig config;
     auto window = std::make_unique<DebugWindow<options, Window>>("Pulsarion Windowing", bounds, styles, config);
     window->SetVisible(true);
@@ -37,26 +38,34 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 #elif defined(PULSARION_EXAMPLES_SHADER_LANGUAGE)
 #endif
 #include "PulsarionCore/Log.hpp"
-#include "PulsarionShaderLanguage/Preprocessor.hpp"
 #include "PulsarionShaderLanguage/Lexer.hpp"
 #include "PulsarionShaderLanguage/Parser.hpp"
+#include "PulsarionShaderLanguage/Preprocessor.hpp"
 #include "PulsarionShaderLanguage/TypeCheck.hpp"
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
 {
     using namespace Pulsarion::Shader;
     std::string path = "resources/shaders/lexer_test.pshl";
     std::string beforePreprocess = Pulsarion::File::ReadAllText(path);
     PULSARION_LOG_INFO("Before preprocess: {}", beforePreprocess);
-    std::string source = Preprocessor(beforePreprocess, path).Process();
+    auto res = Preprocessor(beforePreprocess, path).Process();
+    if (res.HasError())
+    {
+        PULSARION_LOG_ERROR("Preprocessing failed: {0}:{1} - {2}", res.Path.string(), res.Line,
+                            Preprocessor::ErrorToString(res.Err));
+        return 1;
+    }
+    auto source = std::move(res.Source);
     PULSARION_LOG_INFO("After preprocess: {}", source);
     Parser parser(Lexer{source});
     auto result = parser.Parse();
 
     // Display warnings regardless of success
-    for (const auto& warning : result.Warnings)
+    for (auto &warning: result.Warnings)
     {
-        PULSARION_LOG_WARN("[{0}] {1}", Parsing::Error::SourceToString(warning.Source), Parsing::Error::TypeToString(warning.Type));
+        PULSARION_LOG_WARN("[{0}] {1}", Parsing::Error::SourceToString(warning.Source),
+                           Parsing::Error::TypeToString(warning.Type));
     }
 
     if (result.Success())
@@ -68,14 +77,14 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 
         TypeChecker typeChecker(std::move(ast));
         typeChecker.GenerateTypeInformation();
-    }
-    else
+    } else
     {
         PULSARION_LOG_ERROR("Parsing failed");
 
-        for (const auto& error : result.Errors)
+        for (auto &error: result.Errors)
         {
-            PULSARION_LOG_ERROR("[{0}] {1}", Parsing::Error::SourceToString(error.Source), Parsing::Error::TypeToString(error.Type));
+            PULSARION_LOG_ERROR("[{0}] {1}", Parsing::Error::SourceToString(error.Source),
+                                Parsing::Error::TypeToString(error.Type));
         }
     }
 
